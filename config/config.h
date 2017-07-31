e880264401649bb72b6b0d8c27867c9f7032a433
Gustavo Pantuza <gustavopantuza@gmail.com>
2017-07-31 20:08:14 -0300

    Configuration header: Add variables as protected member

--- config/config.h
+++ config/config.h
@@ -1,13 +1,26 @@
 #include <string>
+#include <stdexcept>
+#include <vector>
+#include <map>
 
 
 using namespace std;
 
 
+/*
+ * Generic configuration loader class. Each module inherits from this class
+ * and implements its own set_config method to load as class members variables
+ * that configures the module
+ */
 class ConfigLoader {
 
     public:
-
         void load (string filename);
-        string port;
+
+    protected:
+        /* Class member to store file parsed variables */
+        map<string, vector<string>> variables;
+
+    private:
+        virtual void set_config ();
 };
