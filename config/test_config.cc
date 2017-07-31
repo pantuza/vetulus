ba1276ab7ef48bc872db05af51651e26af850eaf
Gustavo Pantuza <gustavopantuza@gmail.com>
2017-07-31 20:07:22 -0300

    Configuration: Add tests to generic configuration class

--- config/test_config.cc
+++ config/test_config.cc
@@ -1,36 +1,45 @@
 /*
- * =====================================================================================
+ * ============================================================================
  *
  *       Filename:  test_config.cc
  *
- *    Description:  
+ *    Description:  Test program for generic configuration class
  *
- *        Version:  1.0
+ *        Version:  0.1
  *        Created:  07/28/2017 11:07:52 PM
- *       Revision:  none
- *       Compiler:  gcc
+ *       Compiler:  g++
  *
- *         Author:  YOUR NAME (), 
- *   Organization:  
+ *         Author:  Gustavo Pantuza (gustavopantuza@gmail.com)
+ *   Organization:  Computer science community
  *
- * =====================================================================================
+ * ============================================================================
  */
-#include <stdlib.h>
+
+#include <stdexcept>
 
 #include <gtest/gtest.h>
 
 #include "config.h"
 
-// Tests configuration class
-TEST(ConfigurationTest, TestsIf42) {
-    
-    ConfigLoader loader;
-    loader.load("api.conf");
 
-    ASSERT_TRUE(true);
+/* Tests loading a fake file as input */
+TEST(ConfigurationTest, TestFakefileAsInput) {
+
+    ConfigLoader loader;
+    ASSERT_THROW(loader.load("fake.file"), invalid_argument);
 }
 
+
+/* Tests throwing an exception as a not implemented error */
+TEST(ConfigurationTest, TestThrowExceptionOnSetConfigMethod) {
+
+    ConfigLoader loader;
+    ASSERT_THROW(loader.load("api.conf"), runtime_error);
+}
+
+
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
