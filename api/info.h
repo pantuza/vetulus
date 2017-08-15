0dbb1d25b81278f4d27b05ecc17ec663ffb6b393
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-08-14 23:26:00 -0300

    Info API: Start using json module to build response

--- api/info.h
+++ api/info.h
@@ -19,10 +19,17 @@
 #include <pistache/endpoint.h>
 #include <pistache/router.h>
 
+#include "json.hpp"
+
+
+using json = nlohmann::json;
 
 using namespace Pistache;
 
 
+/**
+ * Class to describe the most basic information about the Vetulus API
+ */
 class InfoHandler {
 
     public:
@@ -36,8 +43,21 @@ class InfoHandler {
             );
         }
 
-        void getInfo(const Rest::Request& request, Http::ResponseWriter response) {
 
-             response.send(Http::Code::Ok, "{\"status\": 200, \"body\": \"Info message\"}");
+        /*
+         * Basic Vetulus API information
+         */
+        void getInfo(const Rest::Request& request,
+                     Http::ResponseWriter response) {
+
+            json data;
+            data["server"] = "Vetulus API";
+            data["version"] = {0, 1, 0};
+            data["description"] = "Your data structure as a service";
+            data["project"] = "https://github.com/pantuza/vetulus";
+            data["license"] = "General Public License v3";
+            data["Language"] = "C++11";
+
+            response.send(Http::Code::Ok, data.dump());
         }
 };
