a845eee0af3a22ce4d017ce236a14224add70497
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-11-07 18:03:53 -0200

    VetulusRouter: Add logging support through spdlog for HTTP router

--- api/api.h
+++ api/api.h
@@ -31,7 +31,11 @@ class VetulusRouterHandler: public Http::Handler {
         VetulusRouterHandler(const VetulusRouter& router)
         {
             this->router = router;
-            std::cout << "RouterHandler constructor" << std::endl;
+
+            this->console = spd::get("Router");
+            if(!this->console) {
+                this->console = spd::stdout_color_mt("Router");
+            }
         }
 
         void onRequest(const Http::Request& req, Http::ResponseWriter response)
@@ -39,7 +43,10 @@ class VetulusRouterHandler: public Http::Handler {
             auto resp = response.clone();
             auto result = router.route(req, std::move(resp));
 
-            std::cout << "onRequest log" << std::endl;
+            // Wait for Pull request to be accepted on Pistache to use
+            // the function versionString(req.version()) as parameter
+            this->console->info("{0} {1}",
+                    methodString(req.method()), req.resource());
 
             if (result == Rest::Router::Status::NotFound) {
 
@@ -54,6 +61,7 @@ class VetulusRouterHandler: public Http::Handler {
         }
 
         VetulusRouter router;
+        std::shared_ptr<spd::logger> console;
 };
 
 
