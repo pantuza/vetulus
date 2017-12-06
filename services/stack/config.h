1fdf8d3b78073feff30fe06abce8d83bab678c3b
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-12-06 16:48:13 -0200

    Add Stack Service specific configuration class

---
+++ services/stack/config.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 <Gustavo Pantuza>
+ *
+ * ============================================================================
+ *
+ *       Filename:  config.cc
+ *
+ *    Description:  Vetulus Stack service specific configuration class.
+ *                  Inherits from generic configuration class from
+ *                  config module.
+ *
+ *        Version:  0.1
+ *        Created:  12/05/2017 12:00:32
+ *       Compiler:  g++
+ *
+ *         Author:  Gustavo Pantuza (gustavopantuza@gmail.com)
+ *   Organization:  Computer science community
+ *
+ * ============================================================================
+ */
+
+
+#ifndef SERVICES_STACK_CONFIG_H_
+#define SERVICES_STACK_CONFIG_H_
+
+
+#include <stdlib.h>
+#include <string>
+
+#include "../../config/config.h"
+
+
+using std::string;
+
+
+class StackConfigLoader: public ConfigLoader {
+ public:
+    string port;
+    string addr;
+
+ private:
+    void set_config() {
+        this->port = this->variables["Port"][0];
+        this->addr = this->variables["Addr"][0];
+    }
+};
+
+#endif  // SERVICES_STACK_CONFIG_H_
