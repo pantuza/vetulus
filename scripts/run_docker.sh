d7a6e33d3751baa580edf21b2c9856f3d74144ea
{NAME} <{EMAIL}>
2018-06-07 18:56:15 -0300

    Add script that runs the container

---
+++ scripts/run_docker.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+
+#
+# Build Config
+#
+echo "# Building Configuration library"
+cd /vetulus/config
+mkdir build
+cd build
+cmake -G Ninja ..
+ninja
+
+
+
+#
+# Stack service
+#
+echo "Building Vetulus Stack service"
+cd /vetulus/services/stack
+mkdir -pv build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Coverage
+
+make
+
+echo "Running Vetulus Stack service"
+./stack_server /vetulus/config/files/stack_server.conf &
+
+
+
+#
+# Proto service
+#
+echo "Building Vetulus Proto service"
+cd /vetulus/manager/
+mkdir -pv build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Coverage
+
+make
+
+echo "Running Vetulus Proto service"
+./proto_server /vetulus/config/files/proto_server.conf
