e02e658b8e86ab19858b5bd7c2bdeae54a2db46a
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2018-06-30 16:57:48 -0300

    Only runs binaries. Compilation is done earlier

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,45 +1,8 @@
 #!/bin/bash
 
 
-
-#
-# Build Config
-#
-echo "# Building Configuration library"
-cd /vetulus/config
-mkdir build
-cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Coverage ..
-ninja
-
-
-
-#
-# Stack service
-#
-echo "Building Vetulus Stack service"
-cd /vetulus/services/stack
-mkdir -pv build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Coverage
-
-make
-
 echo "Running Vetulus Stack service"
-./stack_server /vetulus/config/files/stack_server.conf &
+cd /vetulus/services/stack/build/ && ./stack_server /vetulus/config/files/stack_server.conf &
 
-
-
-#
-# Proto service
-#
-echo "Building Vetulus Proto service"
-cd /vetulus/manager/
-mkdir -pv build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Coverage
-
-make
-
-echo "Running Vetulus Proto service"
-./manager_server /vetulus/config/files/manager_server.conf
+echo "Running Vetulus Manager service"
+cd /vetulus/manager/build/ && ./manager_server /vetulus/config/files/manager_server.conf
